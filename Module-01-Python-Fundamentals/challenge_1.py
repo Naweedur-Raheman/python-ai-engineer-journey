ccbf1aa3cd753fe5528fa59feb826241e1c86e0a
Naweedur Raheman <zahidurrevival2@gmail.com>
2026-07-11 21:28:17 +0530

    Add files via upload

---
+++ Module-01-Python-Fundamentals/challenge_1.py
@@ -0,0 +1,5 @@
+Name = input("Enter the name:")
+AI_technology = input("enter your faviourate AI technology:")
+Hours = input("Enter your total studyhours: ")
+
+print(f"Hello {Name}!, Studying {Hours} hours a week will make you an expert in {AI_technology} in no time!")
