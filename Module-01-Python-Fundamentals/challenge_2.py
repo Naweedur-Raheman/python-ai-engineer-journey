c35b8e83a8436b6bbf0773a7ee100d0514f3c4b3
Naweedur Raheman <zahidurrevival2@gmail.com>
2026-07-12 10:21:04 +0530

    Add files via upload

---
+++ Module-01-Python-Fundamentals/challenge_2.py
@@ -0,0 +1,12 @@
+user_mood = input("How are you feeling today: ").lower()
+
+# pyrefly: ignore [parse-error]
+if user_mood == "happy" or user_mood == "excited":
+    print("Thats awesome.Keep smiling!")
+elif user_mood == "sad" or user_mood == "tired":
+    print("I'm sorry to hear that, take a break")
+elif user_mood == "angry" or user_mood == "stressed":
+    print("Take a deep breath. You've got this")
+else:
+    print("Interesting, tell me more about that.")    
+
